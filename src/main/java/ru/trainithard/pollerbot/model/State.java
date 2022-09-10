3b7fab021d913b597b6e3ac4430d73bdedf4ebee
Aleksandr Koshelev <ko4evneg@gmail.com>
2022-09-10 15:57:05 +0300

    - State update

--- src/main/java/ru/trainithard/pollerbot/model/State.java
+++ src/main/java/ru/trainithard/pollerbot/model/State.java
@@ -1,5 +1,15 @@
 package ru.trainithard.pollerbot.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum State {
-    NEW_USER_FIRST_REQUEST, NEW_USER_AWAIT_REGISTRATION
+    NEW_USER_FIRST_REQUEST("Вас приветствует TrainItHard бот! Для использования нужна авторизация. Хотите пройти? (да/нет)"),
+    REGISTRATION_NAMES_STEP("Для регистрации введите Фамилию и Имя (в таком же порядке):"),
+    REGISTRATION_EMAIL_STEP("Введите email для связи и оповещений:"),
+    ;
+
+    private final String text;
 }

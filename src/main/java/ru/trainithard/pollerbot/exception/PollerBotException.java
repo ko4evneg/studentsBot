0c364d036d9293ed2d474bac52f5e7c0f21560c3
Aleksandr Koshelev <ko4evneg@gmail.com>
2022-09-10 11:04:36 +0300

    - Add general PollerBotException - Add BaseEntity

---
+++ src/main/java/ru/trainithard/pollerbot/exception/PollerBotException.java
@@ -0,0 +1,7 @@
+package ru.trainithard.pollerbot.exception;
+
+public class PollerBotException extends RuntimeException{
+    public PollerBotException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/trainithard/pollerbot/model/BaseEntity.java
@@ -0,0 +1,31 @@
+package ru.trainithard.pollerbot.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Objects;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

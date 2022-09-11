b0eefb4ac80a01f60f0308b4251fdc0d49fb0aad
Aleksandr Koshelev <ko4evneg@gmail.com>
2022-09-11 18:34:26 +0300

    - Add user Command

---
+++ src/main/java/ru/trainithard/pollerbot/service/command/ResetCommand.java
@@ -0,0 +1,22 @@
+package ru.trainithard.pollerbot.service.command;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.trainithard.pollerbot.service.dto.UpdateUserSession;
+
+@Component
+@RequiredArgsConstructor
+public class ResetCommand extends AbstractCommand {
+    @Override
+    public SendMessage execute(UpdateUserSession updateUserSession) {
+        updateUserSession.getSession().resetSession();
+        saveSession(updateUserSession);
+        return messageConstructor.construct(getChatId(updateUserSession), "Сессия сброшена!");
+    }
+
+    @Override
+    public CommandName getCommandName() {
+        return CommandName.RESET_COMMAND;
+    }
+}

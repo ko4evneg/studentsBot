533916292036b51125a0424999c400f7e41bf265
Aleksandr Koshelev <ko4evneg@gmail.com>
2022-09-11 18:35:01 +0300

    - Add regular user Command

---
+++ src/main/java/ru/trainithard/pollerbot/service/command/RegularUserGetDataCommand.java
@@ -0,0 +1,31 @@
+package ru.trainithard.pollerbot.service.command;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.trainithard.pollerbot.model.User;
+import ru.trainithard.pollerbot.service.dto.UpdateUserSession;
+
+import java.util.List;
+
+import static ru.trainithard.pollerbot.util.MessageConstructor.Button;
+
+@Component
+public class RegularUserGetDataCommand extends AbstractCommand {
+    @Override
+    public SendMessage execute(UpdateUserSession updateUserSession) {
+        List<Button> lineOne = List.of(new Button("В меню", CommandName.USER_GET_MENU));
+
+        return messageConstructor.constructTextButtons(getChatId(updateUserSession), getUserData(updateUserSession), List.of(lineOne));
+    }
+
+    private String getUserData(UpdateUserSession updateUserSession) {
+        User user = updateUserSession.getUser();
+        return String.format("Ваши данные: \r\n ФИО: %s %s\r\n email: %s \r\n id: %d",
+                user.getFirstName(), user.getLastName(), user.getEmail(), user.getId());
+    }
+
+    @Override
+    public CommandName getCommandName() {
+        return CommandName.USER_GET_DATA;
+    }
+}
---
+++ src/main/java/ru/trainithard/pollerbot/service/command/RegularUserGetMenuCommand.java
@@ -0,0 +1,24 @@
+package ru.trainithard.pollerbot.service.command;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.trainithard.pollerbot.service.dto.UpdateUserSession;
+
+import java.util.List;
+
+import static ru.trainithard.pollerbot.util.MessageConstructor.Button;
+
+@Component
+public class RegularUserGetMenuCommand extends AbstractCommand {
+    @Override
+    public SendMessage execute(UpdateUserSession updateUserSession) {
+        List<Button> buttons = List.of(new Button("Мои данные", CommandName.USER_GET_DATA),
+                new Button("Уроки", CommandName.USER_GET_LESSONS));
+        return messageConstructor.constructTextButtons(getChatId(updateUserSession), "Главное меню:", List.of(buttons));
+    }
+
+    @Override
+    public CommandName getCommandName() {
+        return CommandName.USER_GET_MENU;
+    }
+}

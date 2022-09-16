e48d60179aad5e5bee2f2783aba2a734f80e149d
Aleksandr Koshelev <ko4evneg@gmail.com>
2022-09-16 12:15:02 +0300

    - Add datasource connection - Add liquibase

--- src/main/java/ru/trainithard/pollerbot/BeanConfiguration.java
+++ src/main/java/ru/trainithard/pollerbot/BeanConfiguration.java
@@ -1,10 +1,13 @@
 package ru.trainithard.pollerbot;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.trainithard.pollerbot.service.command.AbstractCommand;
 import ru.trainithard.pollerbot.service.command.CommandName;
 
+import javax.sql.DataSource;
 import java.util.List;
 import java.util.Map;
 
@@ -12,9 +15,28 @@ import static java.util.stream.Collectors.toMap;
 
 @Configuration
 public class BeanConfiguration {
+    @Value(value = "#{environment.dbPassword ?: \"${spring.datasource.password}\"}")
+    private String datasourcePassword;
+    @Value("${spring.datasource.username}")
+    private String datasourceUsername;
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+    @Value("${spring.datasource.driver-class-name}")
+    private String datasourceDriver;
+
     @Bean
     public Map<CommandName, AbstractCommand> commands(List<AbstractCommand> commands) {
         return commands.stream()
                 .collect(toMap(AbstractCommand::getCommandName, command -> command));
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(datasourceDriver);
+        dataSourceBuilder.url(datasourceUrl);
+        dataSourceBuilder.username(datasourceUsername);
+        dataSourceBuilder.password(datasourcePassword);
+        return dataSourceBuilder.build();
     }
 }

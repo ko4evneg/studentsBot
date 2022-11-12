9be6d8b4054c386058e3e794f26c239325a3b47e
ko4evneg <ko4evneg@gmail.com>
2022-11-12 03:48:46 +0300

    - Refactor MessageKeyboard - Add docker postgres image and init-db script

--- src/main/java/ru/trainithard/pollerbot/service/dto/MessageKeyboard.java
+++ src/main/java/ru/trainithard/pollerbot/service/dto/MessageKeyboard.java
@@ -1,32 +1,45 @@
 package ru.trainithard.pollerbot.service.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import ru.trainithard.pollerbot.exception.PollerBotException;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class MessageKeyboard {
     public static final int STANDARD_MESSAGES_END_INDEX = 1;
-    private String[] messages;
+    private String message;
+    private String errorMessage;
+    /**
+     * Markup: each array element represents row. The number in element represents buttons count in that row.
+     */
     private int[] buttonInRowsMarkup;
     private String[] buttonRows;
 
-    public MessageKeyboard(String[] messages) {
-        this.messages = messages;
+    public MessageKeyboard(String...messages) {
+        if (messages.length > 2) {
+            throw new PollerBotException("Wrong arguments count for creating keyboard.");
+        }
+        if (messages.length == 1) {
+            this.message = messages[0];
+        }
+        if (messages.length == 2) {
+            this.errorMessage = messages[1];
+        }
+    }
+
+    public MessageKeyboard(String[] messages, int[] buttonInRowsMarkup, String[] buttonRows) {
+        this(messages);
+        this.buttonInRowsMarkup = buttonInRowsMarkup;
+        this.buttonRows = buttonRows;
     }
 
     public String getMessage() {
-        return messages[0];
+        return message;
     }
 
     public String getErrorMessage() {
-        return messages[1];
-    }
-
-    public String getCustomMessage(int number) {
-        return messages[STANDARD_MESSAGES_END_INDEX + number];
+        return errorMessage;
     }
 
     public boolean hasButtons() {
